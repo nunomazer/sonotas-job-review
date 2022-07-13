977879ec9faf5f3253a73a14fc3b75f090b05b05
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-07-12 21:51:28 -0300

    migration empresa integração

---
+++ database/migrations/2022_07_09_211521_create_empresa_integras_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpresaIntegrasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresa_integracoes', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('empresa_id');
+            $table->foreign('empresa_id')->references('id')->on('empresas');
+            $table->string('name');
+            $table->string('platform');
+            $table->jsonb('fields');
+            $table->boolean('ativo')->default(true);
+            $table->string('tipo_documento');
+            $table->date('data_inicio');
+            $table->boolean('transmissao_automatica')->default(false);
+            $table->string('transmissao_periodo');
+            $table->boolean('transmissao_apenas_dias_uteis')->default(false);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresa_integras');
+    }
+}
