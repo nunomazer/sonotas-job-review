6d43ccbacc493bff9feb312ade25fb62c4726b27
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-07-16 12:07:54 -0300

    enum de tipos de logradouros

---
+++ app/Models/TipoLogradouro.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+class TipoLogradouro
+{
+    const tipos = [
+        'Alameda',
+        'Avenida',
+        'Chácara',
+        'Colônia',
+        'Condomínio',
+        'Eqnp',
+        'Estância',
+        'Estrada',
+        'Fazenda',
+        'Praça',
+        'Prolongamento',
+        'Rodovia',
+        'Rua',
+        'Sítio',
+        'Travessa',
+        'Vicinal',
+    ];
+}
