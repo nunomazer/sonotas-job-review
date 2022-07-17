a2335102fbdcb67d7744ff609c3179034a9fbc59
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-07-17 11:45:43 -0300

    melhor padronização dos retornos padronizados

--- app/Services/Sped/SpedApiReturn.php
+++ app/Services/Sped/SpedApiReturn.php
@@ -4,19 +4,29 @@ namespace App\Services\Sped;
 
 class SpedApiReturn
 {
-    protected $code;
-    protected $message;
-    protected bool $error = false;
-    protected array $data = [];
+    public $code;
+    public $message;
+    public bool $error = false;
+    public array $data = [];
 
-    public function __get($name)
-    {
-        if ($name == 'code') return $this->code;
+    /**
+     * Protocolo de comunicação retornado pela API, para algumas operações
+     * @var $protocol
+     */
+    public $protocol;
 
-        if ($name == 'message') return $this->message;
-
-        if ($name == 'error') return $this->error;
-
-        if ($name == 'data') return $this->data;
-    }
+    /**
+     * Array, de objetos gerados na api integradora do Sped. Cada objeto deve identificar o seu código (ou id), ou possuir
+     * mais dados de acordo com a operação.
+     * É necessário que garanta a identificação de retorno principalmente quando enviamos documentos discais em uma chamada
+     * com lote de documentos.
+     * O retorno deve identificar:
+     * <ul>
+     * <li><strong>idSoNotas</strong>: nosso id enviado para a api</li>
+     * <li><strong>documento</strong>: CPF ou CNPJ do prestador</li>
+     * <li><strong>idApi</strong>: id gerado pela api para identificação do objeto, que será gravado junto ao registro do documento fiscal para futuras consultas na api</li>
+     * </ul>
+     * @var array $objects
+     */
+    public $objects;
 }
