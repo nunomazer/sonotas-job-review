55d4bb19bab3e204c47015476b93bb9b5bd16985
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-07-19 08:26:23 -0300

    contoller e request iniciais para serviços

---
+++ app/Http/Requests/ServicoRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\TipoLogradouro;
+use App\Services\Sped\RegimesTributarios;
+use App\Services\Sped\RegimesTributariosEspeciais;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ServicoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'ativo' => filter_var($this->request->get('ativo'), FILTER_VALIDATE_BOOLEAN),
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'empresa_id'            => ['required'],
+            'nome'                  => ['required'],
+            'descricao'             => ['required'],
+            'valor'                 => ['required'],
+            'tipo_servico_codigo'   => ['required'],
+        ];
+    }
+}
