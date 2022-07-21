3fd31d4e5cce336e57afe3914542d777fd112d54
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-07-21 20:59:51 -0300

    refactor: resolve validações das rotinas

--- app/Services/TipoLogradouroService.php
+++ app/Services/TipoLogradouroService.php
@@ -12,14 +12,14 @@ class TipoLogradouroService
      * Valida um tipo de logradouro, caso não seja válido resolve para um dos tipos pelo endereço informado, ou retorna o valor padrão.
      *
      * @param string $tipo
-     * @param string $endereco
+     * @param string $logradouro
      * @param string $default
      * @return string
      */
-    public function validoOuResolve(string $tipo, string $endereco, string $default = 'Rua') : string
+    public function validoOuResolve(string $tipo, string $logradouro, string $default = 'Rua') : string
     {
        if (in_array(Str::lower($tipo), Str::lower(TipoLogradouro::tipos)) === false) {
-           return $this->resolvePeloEndereco($endereco, $default);
+           return $this->resolvePeloLogradouro($logradouro, $default);
        }
 
        return $tipo;
@@ -28,14 +28,14 @@ class TipoLogradouroService
     /**
      * Resolve o tipo do logradouro pelo endereço passado, caso não consiga devolve o valor padrão do segundo parâmetro.
      *
-     * @param string $endereco
+     * @param string $logradouro
      * @param string $default
      * @return string
      */
-    public function resolvePeloEndereco(string $endereco, string $default = 'Rua') : string
+    public function resolvePeloLogradouro(?string $logradouro, string $default = 'Rua') : string
     {
         foreach (TipoLogradouro::tipos as $tipo) {
-            if (Str::contains(Str::lower($endereco), Str::lower($tipo))) {
+            if (Str::contains(Str::lower($logradouro), Str::lower($tipo))) {
                 return $tipo;
             }
         }
