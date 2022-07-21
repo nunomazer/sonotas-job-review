e1385d05dacb44c4d51010412192e78ff0e4ba5f
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-07-21 20:49:30 -0300

    feat: valida um tipo de logradouro ou resolve pelo endereço

--- app/Services/TipoLogradouroService.php
+++ app/Services/TipoLogradouroService.php
@@ -9,6 +9,23 @@ class TipoLogradouroService
 {
 
     /**
+     * Valida um tipo de logradouro, caso não seja válido resolve para um dos tipos pelo endereço informado, ou retorna o valor padrão.
+     *
+     * @param string $tipo
+     * @param string $endereco
+     * @param string $default
+     * @return string
+     */
+    public function validoOuResolve(string $tipo, string $endereco, string $default = 'Rua') : string
+    {
+       if (in_array(Str::lower($tipo), Str::lower(TipoLogradouro::tipos)) === false) {
+           return $this->resolvePeloEndereco($endereco, $default);
+       }
+
+       return $tipo;
+    }
+
+    /**
      * Resolve o tipo do logradouro pelo endereço passado, caso não consiga devolve o valor padrão do segundo parâmetro.
      *
      * @param string $endereco
