abbe7c3333206fee1f05a26d1f1afe6b28905ec8
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-07-21 21:40:24 -0300

    resolvedor de city_id

---
+++ app/Services/CidadeService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Cidade;
+use App\Models\TipoLogradouro;
+use Illuminate\Support\Str;
+
+class CidadeService
+{
+    /**
+     * Resolve o id da cidade pelo nome ou retorna o id da cidade default. Se não encontrar nenhuma retorna 1
+     *
+     * @param string $nomeCidade
+     * @param string $default
+     * @return string
+     */
+    public function resolveIdPeloNome(?string $nomeCidade, string $default = 'São Paulo') : int
+    {
+        $cidade = Cidade::where('name', $nomeCidade)->first();
+
+        if ($cidade) return $cidade->id;
+
+        $cidade = Cidade::where('name', $default)->first();
+
+        if ($cidade) return $cidade->id;
+
+        return 1;
+    }
+}
