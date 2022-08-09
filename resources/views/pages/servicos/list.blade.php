4322ccd155c2a44af0efda687b461cb36fb74655
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-08-09 10:10:29 -0300

    refactor: comentado possível menu de importar serviços

--- resources/views/pages/servicos/list.blade.php
+++ resources/views/pages/servicos/list.blade.php
@@ -17,6 +17,33 @@
 
                     Novo serviço
                 </a>
+
+<!--                <div class="dropdown">
+                    <button class="btn btn-sm btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-replace" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <rect x="3" y="3" width="6" height="6" rx="1"></rect>
+                            <rect x="15" y="15" width="6" height="6" rx="1"></rect>
+                            <path d="M21 11v-3a2 2 0 0 0 -2 -2h-6l3 3m0 -6l-3 3"></path>
+                            <path d="M3 13v3a2 2 0 0 0 2 2h6l-3 -3m0 6l3 -3"></path>
+                        </svg>
+
+                        Importar Serviços
+                    </button>
+                    <ul class="dropdown-menu">
+                        @if($integracoes->count())
+                            <li><a class="dropdown-item" href="#">Todas as plataformas</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            @foreach($integracoes as $integracao)
+                                <li><a class="dropdown-item" href="#">{{ $integracao->driver }}</a></li>
+                            @endforeach
+                        @else
+                            <p>
+                                Nenhuma integração configurada
+                            </p>
+                        @endif
+                    </ul>
+                </div>-->
             </div>
         </div>
         <div class="card-body">
