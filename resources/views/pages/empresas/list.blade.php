f7b6ba8ce92364aebb5c0f91a929f0300a0cda8c
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-08-09 11:08:49 -0300

    refactor: redesenha para painel a área de empresas

--- resources/views/pages/empresas/list.blade.php
+++ resources/views/pages/empresas/list.blade.php
@@ -20,23 +20,21 @@
             </div>
         </div>
         <div class="card-body">
-            <div id="table-default" class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th class="table-sort" data-sort="sort-name">Nome</th>
-                        <th>
-                            Plano
-                        </th>
-                        <th>Configuração NFSe</th>
-                        <th>Integrações</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody class="table-tbody">
-                        @foreach($empresas as $empresa)
-                            <tr>
-                                <td>
+
+            @foreach($empresas as $empresa)
+                <div class="card d-flex flex-column">
+                    <div class="row row-0 flex-fill">
+                        <div class="col-md-2">
+                            <a href="#">
+                                <div class="ratio ratio-1x1 placeholder">
+                                    <div class="placeholder-image"></div>
+                                </div>
+    {{--                            <img src="..." class="w-100 h-100 object-cover" alt="Card side image">--}}
+                            </a>
+                        </div>
+                        <div class="col">
+                            <div class="card-body row">
+                                <h3 class="card-title col-8">
                                     <span class="mx-1 {{ $empresa->ativo ? 'status-green' : '' }}">
                                         <span class="status-dot"></span>
                                     </span>
@@ -44,59 +42,96 @@
                                     <a href="{{route('empresas.edit', $empresa)}}">
                                         {{ $empresa->nome }}
                                     </a>
-                                </td>
-                                <td>
-                                    @include('pages.empresas.partials.assinatura-status')
-                                </td>
-                                <td>
+                                </h3>
+                                <div class="col-2">
                                     @if($empresa->configuracao_nfse)
                                         <span class="mx-1 status-green">
                                             <span class="status-dot"></span>
                                         </span>
-                                        <a href="{{route('empresas.nfse.edit', [$empresa, $empresa->configuracao_nfse])}}">
-                                            Dados NFSe
+                                        <a href="{{route('empresas.nfse.edit', [$empresa, $empresa->configuracao_nfse])}}"
+                                            class="btn btn-sm btn-info">
+                                            Configurar NFSe
                                         </a>
                                     @else
                                         <span class="mx-1 status-warning">
                                             <span class="status-dot"></span>
                                         </span>
-                                        <a href="{{route('empresas.nfse.create', [$empresa])}}">
-                                            Configurar
+                                        <a href="{{route('empresas.nfse.create', [$empresa])}}"
+                                           class="btn btn-sm btn-warning">
+                                            Configurar NFSe
                                         </a>
                                     @endif
-                                </td>
-                                <td>
-                                    @forelse($empresa->integracoes as $integracao)
-                                        <span class="mx-1 {{ $integracao->ativo ? 'status-green' : '' }}">
-                                            <span class="status-dot"></span>
-                                        </span>
-                                        <a href="{{ route('empresas.integracoes.edit', [$empresa, $integracao]) }}">
-                                            {{ $integracao->driver }}
-                                        </a>
-                                    @empty
-                                        <span class="mx-1 status-warning">
-                                            <span class="status-dot"></span>
-                                        </span>
-                                        Nenhuma integração
-                                    @endforelse
-                                </td>
-                                <td>
-                                    <a href="{{route('empresas.edit', $empresa)}}" class="btn btn-sm">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                            <circle cx="12" cy="12" r="9"></circle>
-                                            <line x1="9" y1="12" x2="15" y2="12"></line>
-                                            <line x1="12" y1="9" x2="12" y2="15"></line>
-                                        </svg>
+                                </div>
 
-                                        Nova integração
-                                    </a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                                <div class="col-2">
+                                    @include('pages.empresas.partials.assinatura-status')
+                                </div>
+
+                                <div class="col-3 mt-3">
+                                    <div class="ms-3">
+                                        <a href="{{route('empresas.edit', $empresa)}}" class="btn btn-sm">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                <circle cx="12" cy="12" r="9"></circle>
+                                                <line x1="9" y1="12" x2="15" y2="12"></line>
+                                                <line x1="12" y1="9" x2="12" y2="15"></line>
+                                            </svg>
+
+                                            Nova integração
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div id="table-default" class="table-responsive mt-3">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th class="table-sort" data-sort="sort-name">Integração</th>
+                                                <th>Vendas</th>
+                                                <th>Serviços</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="table-tbody">
+                                            @forelse($empresa->integracoes as $integracao)
+                                                <tr>
+                                                    <td>
+                                                        <span class="mx-1 {{ $integracao->ativo ? 'status-green' : '' }}">
+                                                            <span class="status-dot"></span>
+                                                        </span>
+                                                        <a href="{{ route('empresas.integracoes.edit', [$empresa, $integracao]) }}">
+                                                            {{ $integracao->driver }}
+                                                        </a>
+                                                    </td>
+
+                                                    <td>
+                                                        X Vendas
+                                                    </td>
+
+                                                    <td>
+                                                        X Serviços
+                                                    </td>
+
+                                                    <td>
+                                                        <form method="POST" action="{{route('servicos.importar', $integracao)}}"
+                                                    </td>
+
+                                                </tr>
+                                            @empty
+                                                <span class="mx-1 status-warning">
+                                                    <span class="status-dot"></span>
+                                                </span>
+                                                Nenhuma integração
+                                            @endforelse
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
