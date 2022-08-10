86e43b1f7339cc4a2e2195688ee25be18233d9bd
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-08-09 22:42:08 -0300

    feat: schedule para importar todas integrações de hora em hora

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('sync:vendas-plataformas')->hourly();
+
         $schedule->command('ibge:import-states-cities')->monthly();
     }
 
