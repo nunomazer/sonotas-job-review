76c895f0d4ad2e31964e2f06690a70352a00456c
Bruno Duarte <duartebruno2208@gmail.com>
2022-09-09 16:44:16 -0300

    fix bug botão sair

--- resources/views/layouts/partials/dropdown-user.blade.php
+++ resources/views/layouts/partials/dropdown-user.blade.php
@@ -21,7 +21,7 @@
         </a>
         <div class="dropdown-divider"></div>
         <form action="{{ route('logout') }}" method="POST">
-            <button type="submit" class="dropdown-item d-none d-sm-inline">
+            <button type="submit" class="dropdown-item  d-sm-inline">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
