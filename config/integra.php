4b2a21ecc5f1cc83deacc704ae319ed84d1d5801
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2022-11-15 21:06:09 -0300

    fix: usa config pois env pode não ser carregado
    Pq .env é cacheado e pode permanecer com dados antigos

--- config/integra.php
+++ config/integra.php
@@ -9,6 +9,8 @@ return [
         'eduzz' => [
             'base_url' => env('EDUZZ_BASE_URL','https://api-devhub.eduzz.com'),
             'base_class' => \App\Services\Integra\Drivers\Eduzz\EduzzPlatform::class,
+            'app_id' => env('EDUZZ_APP_ID', '09123'),
+            'redirect_url' => env('EDUZZ_REDIRECT_URL', '12309'),
         ],
     ],
 
