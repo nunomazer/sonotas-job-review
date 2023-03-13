b189d903ed4243dfdb66780335e44b0db253f407
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2023-03-12 21:30:39 -0300

    feat: api configuracao nfse
    AB#247

--- tests/Feature/Api/EmpresaTest.php
+++ tests/Feature/Api/EmpresaTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Feature\Api;
 
 use App\Models\Empresa;
+use App\Models\EmpresaNFSConfig;
 use App\Models\User;
 use App\Models\UserEmpresa;
 use App\Services\EmpresaService;
+use Database\Factories\EmpresaNFSConfigFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
@@ -37,18 +39,53 @@ class EmpresaTest extends TestCase
         $response
             ->assertStatus(Response::HTTP_CREATED);
 
-
         $empresa = Empresa::where('documento', $empresa->documento)->first();
         UserEmpresa::where('empresa_id', $empresa->id)->delete();
         $empresa->delete();
     }
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+
     public function test_config_nfse_criada_nova()
     {
-        $this->markAsRisky();
+        $empresa = Empresa::factory()->make();
+        $empresa->owner_user_id = $this->user->id;
+        $empresa = (new EmpresaService())->create($empresa->toArray());
+
+        $configNfse = EmpresaNFSConfig::factory()->make();
+        $configNfse->empresa_id = $empresa->id;
+
+        Sanctum::actingAs($this->user);
+        $response = $this->putJson('/api/empresas/'.$empresa->id.'/configuracao-nfse', $configNfse->toArray());
+
+        // criou
+        $response->assertStatus(Response::HTTP_OK);
+
+        EmpresaNFSConfig::where('empresa_id', $empresa->id)->delete();
+        UserEmpresa::where('empresa_id', $empresa->id)->delete();
+        $empresa->delete();
+    }
+
+    public function test_config_nfse_atualiza_sem_duplicar()
+    {
+        $empresa = Empresa::factory()->make();
+        $empresa->owner_user_id = $this->user->id;
+        $empresa = (new EmpresaService())->create($empresa->toArray());
+
+        $configNfse = EmpresaNFSConfig::factory()->make();
+        $configNfse->empresa_id = $empresa->id;
+        $configNfse->save();
+
+        $configNfse->cofins = 99.17;
+
+        Sanctum::actingAs($this->user);
+        $response = $this->putJson('/api/empresas/'.$empresa->id.'/configuracao-nfse', $configNfse->toArray());
+
+        // atualizou
+        $response->assertStatus(Response::HTTP_OK);
+
+        $this->assertEquals(1, UserEmpresa::where('empresa_id', $empresa->id)->count());
+
+        EmpresaNFSConfig::where('empresa_id', $empresa->id)->delete();
+        UserEmpresa::where('empresa_id', $empresa->id)->delete();
+        $empresa->delete();
     }
 }
